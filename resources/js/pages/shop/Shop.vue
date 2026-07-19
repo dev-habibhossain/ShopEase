e8881a3bf869bcb2b5fbab54da6742d32b315338
Md Fahim <habibfahim71@gmail.com>
2026-07-20 01:00:53 +0600

    update shop for marge github

--- resources/js/pages/shop/Shop.vue
+++ resources/js/pages/shop/Shop.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, watch } from 'vue';
+import { ref, computed } from 'vue';
 import { Head, Link, router } from '@inertiajs/vue3';
 import { shop } from '@/routes';
 import { useCart } from '@/composables/useCart';
